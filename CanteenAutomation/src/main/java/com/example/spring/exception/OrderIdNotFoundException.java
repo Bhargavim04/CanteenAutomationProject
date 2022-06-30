20f57134abaa839bfa184c54f6cd54119a7667ee
Gade <nagamani.gade@capgemini.com>
2022-06-30 12:05:43 +0530

    added order

---
+++ CanteenAutomation/src/main/java/com/example/spring/exception/OrderIdNotFoundException.java
@@ -0,0 +1,24 @@
+package com.example.exception;
+
+public class OrderIdNotFoundException {
+	 private static final long serialVersionUID = 1L;
+	  private String errorCode;
+	  private String errorMessage;
+	  public String getErrorCode() {
+	    return errorCode;
+	  }
+	  public void setErrorcode(String errorCode) {
+	    this.errorCode = errorCode;
+	  }
+	  public String getErrorMessage() {
+	    return errorMessage;
+	  }
+	  public void setErrorMessage(String errorMessage) {
+	    this.errorMessage = errorMessage;
+	  }
+	  public OrderIdNotFoundException(String errorCode,String errorMessage) {
+	    super();
+	    this.errorCode=errorCode;
+	    this.errorMessage=errorMessage;
+	  }
+}
