79f1010b0a78b2700500c3ce86fca62b06c1cfd5
Panumati <saisudha.panumati@capgemini.com>
2022-06-30 11:20:11 +0530

    added food

---
+++ CanteenAutomation/src/main/java/com/example/spring/exception/CanteenFoodExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.spring.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.example.spring.entity.ErrorResponse;
+@ControllerAdvice
+public class CanteenFoodExceptionHandler {
+	
+	@ExceptionHandler(CanteenFoodNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleException(CanteenFoodNotFoundException exception){
+		ErrorResponse error=new ErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exception.getMessage());
+		error.setTimeStamp(LocalDateTime.now());
+		
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+	}
+
+
+}
